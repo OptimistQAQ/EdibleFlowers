dbd0f6963f516206e243ad5573a90f46ad7ef20f
OptimistQAQ <656673477@qq.com>
2021-01-30 20:48:59 +0800

    :memo: Update MainActivity

--- app/src/main/java/com/example/edibleflowers/MainActivity.java
+++ app/src/main/java/com/example/edibleflowers/MainActivity.java
@@ -1,13 +1,64 @@
 package com.example.edibleflowers;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private long time = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestPermissions();
     }
+
+    @Override
+    public void onBackPressed() {
+        if (System.currentTimeMillis() - time > 2000) {
+            Toast.makeText(MainActivity.this, "再按一次 退出应用", Toast.LENGTH_SHORT).show();
+            time = System.currentTimeMillis();
+        } else {
+            finish();
+        }
+    }
+
+    //权限申请
+    private void requestPermissions() {
+        //申请权限
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            Window window = getWindow();
+            window.addFlags( WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS );
+            window.setStatusBarColor( getResources().getColor( R.color.white) );
+            window.getDecorView().setSystemUiVisibility( View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR  );
+        }
+        String[] permissions = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.RECORD_AUDIO, Manifest.permission.READ_CONTACTS, Manifest.permission.CALL_PHONE,
+                Manifest.permission.SEND_SMS,Manifest.permission.CAMERA};
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                for(int i = 0;i < permissions.length;i ++) {
+                    int permission = ActivityCompat.checkSelfPermission( this, permissions[i]);
+                    if (permission != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions( this,permissions, 0x0010 );
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/example/edibleflowers/fragment/HomeFragment.java
@@ -0,0 +1,6 @@
+package com.example.edibleflowers.fragment;
+
+import android.support.v4.app.Fragment;
+
+public class HomeFragment extends Fragment {
+}
