a4a21d683f935b7ec2779ccbcb84e1fd3d55c6a3
AimerNeige <aimer.neige.soft@gmail.com>
2021-02-02 16:26:51 +0800

    update fragment

--- app/src/main/java/com/example/edibleflowers/fragment/HomeFragment.java
+++ app/src/main/java/com/example/edibleflowers/fragment/HomeFragment.java
@@ -1,6 +1,30 @@
 package com.example.edibleflowers.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.edibleflowers.R;
 
 public class HomeFragment extends Fragment {
+
+    private View root;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        root = inflater.inflate(R.layout.fragment_home, container, false);
+        initView();
+        return root;
+    }
+
+
+    private void initView()
+    {
+
+    }
 }
--- app/src/main/java/com/example/edibleflowers/fragment/MineFragment.java
+++ app/src/main/java/com/example/edibleflowers/fragment/MineFragment.java
@@ -1,6 +1,51 @@
 package com.example.edibleflowers.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-public class MineFragment extends Fragment {
+import com.example.edibleflowers.R;
+
+public class MineFragment extends Fragment implements View.OnClickListener {
+
+    private View root;
+    private ImageButton mine_settings;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        root = inflater.inflate(R.layout.fragment_mine, container, false);
+        initView();
+        return root;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    private void initView()
+    {
+        mine_settings = root.findViewById(R.id.mine_settings);
+        mine_settings.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.mine_settings:
+                Toast.makeText(getActivity(), "Settigs", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                Log.d("MineFrament", "OnClick Default");
+                break;
+        }
+    }
 }
