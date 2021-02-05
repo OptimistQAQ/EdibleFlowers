fa55327f5d2ecc48c15655518db13b65e84cf9d7
AimerNeige <aimer.neige.soft@gmail.com>
2021-02-05 22:09:01 +0800

    tab success

--- app/src/main/java/com/example/edibleflowers/fragment/ZiXunFragment.java
+++ app/src/main/java/com/example/edibleflowers/fragment/ZiXunFragment.java
@@ -1,9 +1,13 @@
 package com.example.edibleflowers.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,10 +17,15 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.edibleflowers.R;
 import com.example.edibleflowers.bean.BannerInfo;
+import com.example.edibleflowers.fragment.zixun.ZixunClassificationFragment;
+import com.example.edibleflowers.fragment.zixun.ZixunHighqualityFragment;
+import com.example.edibleflowers.fragment.zixun.ZixunNewsFragment;
+import com.flyco.tablayout.SlidingTabLayout;
+import com.flyco.tablayout.listener.OnTabSelectListener;
+import com.flyco.tablayout.widget.MsgView;
 import com.stx.xhb.xbanner.XBanner;
 import com.stx.xhb.xbanner.transformers.Transformer;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,6 +33,11 @@ public class ZiXunFragment extends Fragment {
 
     private View root;
     private XBanner xBanner;
+
+    private ArrayList<Fragment> mFragments = new ArrayList<>();
+    private ViewPager mViewPager;
+    private SlidingTabLayout mTabLayout;
+    private final String[] mTitles = {"精选", "分类", "新闻"};
 
     // 测试用图片 url
     private final String baiduLogoImageUrl = "https://www.baidu.com/img/flexible/logo/pc/result@2.png";
@@ -46,7 +60,38 @@ public class ZiXunFragment extends Fragment {
     private void initView()
     {
         xBanner = root.findViewById(R.id.zixun_banner);
-//        精选 分类 新闻
+
+        mFragments.add(new ZixunHighqualityFragment());    // 精选
+        mFragments.add(new ZixunClassificationFragment()); // 分类
+        mFragments.add(new ZixunNewsFragment());           // 新闻
+
+        mViewPager = root.findViewById(R.id.zixun_view_pager);
+        mViewPager.setAdapter(new MyPageAdapter(getFragmentManager()));
+
+        mTabLayout = root.findViewById(R.id.zixun_sliding_tab_layout);
+        mTabLayout.setViewPager(mViewPager, mTitles, requireActivity(), mFragments);
+
+        mViewPager.setCurrentItem(0);
+        mTabLayout.showDot(0); // 小红点
+        mTabLayout.showMsg(1, 5); // 消息数量
+        mTabLayout.setMsgMargin(1, 0, 4); // 调整消息小红点的位置
+        // 设置消息小红点的颜色
+        MsgView tabLayoutMsgView = mTabLayout.getMsgView(1);
+        if (tabLayoutMsgView != null) {
+            tabLayoutMsgView.setBackgroundColor(Color.parseColor("#6D8FB0"));
+        }
+
+        mTabLayout.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelect(int position) {
+                mViewPager.setCurrentItem(position);
+            }
+
+            @Override
+            public void onTabReselect(int position) {
+                mViewPager.setCurrentItem(position);
+            }
+        });
     }
 
     /**
@@ -111,5 +156,31 @@ public class ZiXunFragment extends Fragment {
     public void onStop() {
         super.onStop();
         xBanner.stopAutoPlay();
+    }
+
+    /**
+     * fragment 适配器
+     */
+    private class MyPageAdapter extends FragmentPagerAdapter {
+        public MyPageAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mTitles[position];
+        }
     }
 }
