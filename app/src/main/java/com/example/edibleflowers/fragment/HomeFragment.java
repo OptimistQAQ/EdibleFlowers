bfd44008840debb5d36b273d270850208c584bf1
AimerNeige <aimer.neige.soft@gmail.com>
2021-02-06 14:30:49 +0800

    new view

--- app/src/main/java/com/example/edibleflowers/fragment/HomeFragment.java
+++ app/src/main/java/com/example/edibleflowers/fragment/HomeFragment.java
@@ -22,12 +22,6 @@ import java.util.List;
 public class HomeFragment extends Fragment {
 
     private View root;
-    private XBanner xBanner;
-
-    // 测试用图片 url
-    private final String baiduLogoImageUrl = "https://www.baidu.com/img/flexible/logo/pc/result@2.png";
-    private final String bingDailyImageUrl = "https://cn.bing.com/th?id=OHR.VosgesBioReserve_ZH-CN4762694302_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp";
-    private final String appleImageUrl = "https://www.apple.com.cn/home/heroes/cny-2021-film/images/cny__gaectlu0tiai_mediumtall.jpg";
 
     @Nullable
     @Override
@@ -35,7 +29,6 @@ public class HomeFragment extends Fragment {
         root = inflater.inflate(R.layout.fragment_home, container, false);
         initView();
         initData();
-        initBanner();
         return root;
     }
 
@@ -44,60 +37,16 @@ public class HomeFragment extends Fragment {
      */
     private void initView()
     {
-        xBanner = root.findViewById(R.id.home_banner);
+
 
     }
 
-    /**
-     * 初始化轮播 banner
-     * https://github.com/xiaohaibin/XBanner
-     */
-    private void initBanner()
-    {
-        // 设置图片点击事件
-        xBanner.setOnItemClickListener(new XBanner.OnItemClickListener() {
-            @Override
-            public void onItemClick(XBanner banner, Object model, View view, int position) {
-                Toast.makeText(getContext(),
-                        "点击了第" + (position + 1) + "图片  " + ((BannerInfo) model).getTitle(),
-                        Toast.LENGTH_SHORT)
-                        .show();
-            }
-        });
-        // 加载图片
-        xBanner.loadImage(new XBanner.XBannerAdapter() {
-            @Override
-            public void loadBanner(XBanner banner, Object model, View view, int position) {
-                BannerInfo bannerImage = ((BannerInfo) model);
-                String url = bannerImage.getXBannerUrl();
-                Glide.with(HomeFragment.this)
-                        .load(url)
-                        .into((ImageView) view);
-            }
-        });
-        xBanner.setPageTransformer(Transformer.Alpha);
-
-    }
 
     /**
      * 初始化数据
      */
     private void initData()
     {
-        // 连接服务器获取 banner 信息 下面数据用于演示
 
-
-        List<BannerInfo> demoData = new ArrayList<>();
-
-        BannerInfo baidu = new BannerInfo(baiduLogoImageUrl, "Baidu");
-        BannerInfo bing  = new BannerInfo(bingDailyImageUrl, "Bing");
-        BannerInfo apple = new BannerInfo(appleImageUrl, "Apple");
-
-        demoData.add(baidu);
-        demoData.add(bing);
-        demoData.add(apple);
-
-        xBanner.setBannerData(demoData);
-        xBanner.setAutoPlayAble(demoData.size() > 1);
     }
 }
